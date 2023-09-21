d988e70f2e877548739febd264325d676663a10e
iand-casa <ian.diaz.7e5@itb.cat>
2023-09-21 20:19:18 +0200

    Practica 1

--- UF1/HelloWorld.py
+++ UF1/HelloWorld.py
@@ -4,4 +4,5 @@ M03 UF1
 19/9/2023
 Exemple
 """
+# Mostrem "Hello World" a l'output
 print("Hello World")
---
+++ UF1/edat.py
@@ -0,0 +1,15 @@
+"""
+Ian Diaz
+M03 UF1
+21/9/2023
+Exemple
+"""
+
+# Definició de la variable edat proporcionada per l'usuari com a int
+edat = int(input("Introdueix la teva edat\n"))
+
+# Comparem el valor de la variable "edat" amb 18. Depenent de si es major o igual, o menor mostrem un output diferent.
+if edat >= 18:
+    print("Ets major d'edat")
+else:
+    print("Pa tu casa, a ver pocoyo")
