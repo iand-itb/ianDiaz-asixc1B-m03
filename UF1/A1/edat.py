d627db76cf9c56b0ab5684db55e60ba18fe4d187
iand-itb <ian.diaz.7e5@itb.cat>
2023-09-27 12:55:45 +0200

    codigo de makinon

--- UF1/A1/edat.py
+++ UF1/A1/edat.py
@@ -6,10 +6,16 @@ Exemple
 """
 
 # Definició de la variable edat proporcionada per l'usuari com a int
-edat = int(input("Introdueix la teva edat\n"))
+try:
+    edat = int(input("Introdueix la teva edat\n"))
 
 # Comparem el valor de la variable "edat" amb 18. Depenent de si es major o igual, o menor mostrem un output diferent.
-if edat >= 18:
-    print("Ets major d'edat")
-else:
-    print("Pa tu casa, a ver pocoyo")
+    if edat >= 18:
+        print("Ets major d'edat")
+    else:
+        print("Pa tu casa, a ver pocoyo")
+# Si el codi dins de "try: " falla, s'executa l'except i surt del programa amb codi "1" (error)s
+except Exception as e:
+    print("Introdueix una edat vàlida... Error: ")
+    print(e)
+    exit(1)
