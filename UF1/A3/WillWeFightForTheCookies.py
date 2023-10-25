232a80bb194c0fd085109f8fad3927c551d48ec0
iand-itb <ian.diaz.7e5@itb.cat>
2023-10-25 12:47:14 +0200

    willwefightforthecookies

---
+++ UF1/A3/WillWeFightForTheCookies.py
@@ -0,0 +1,13 @@
+'''
+Ian Díaz
+M03 UF1
+Mostrar el numero de dias de un mes
+25/10/2023
+'''
+
+uinput = input()
+cookiePeople = uinput.split(" ")
+if cookiePeople[0] != cookiePeople[1]:
+    print("Let's fight!")
+else:
+    print("Let's eat!")
