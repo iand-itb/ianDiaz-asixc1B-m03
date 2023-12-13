8f2ee7fd4ee795f8b4ba4901d88e63e469951463
iand-casa <ian.diaz.7e5@itb.cat>
2023-12-13 13:04:03 +0100

    retry on except

--- UF1/A4/e21.py
+++ UF1/A4/e21.py
@@ -1,20 +1,30 @@
 import random
-CORRECTO = 0
-guess = random.randint(1, 100)
-top = 100
-bot = 1
-intentos = 0
-while CORRECTO < 1 or CORRECTO > 100:
-    CORRECTO = int(input("Introduce un número a adivinar entre el 1 y el 100: "))
-while guess != CORRECTO and intentos < 10:
-    ask = input(f"És més gran que {guess} ?\n")
-    if ask == "si":
-        bot = guess
-    elif ask == "no":
-        top = guess
-    guess = random.randint(bot + 1, top - 1)
-    intentos +=1
-if guess == CORRECTO:
-    print(f"He adivinado el número ({guess}), y me han sobrado {10 - intentos} intentos.")
-if intentos >= 10:
-    print("Te has quedado sin intentos")
+#bucle while con try except para reintentar si ocurre un ValueError
+while True:
+    try:
+        CORRECTO = 0
+        guess = random.randint(1, 100)
+        top = 100
+        bot = 1
+        intentos = 0
+        while CORRECTO < 1 or CORRECTO > 100:
+            CORRECTO = int(input("Introduce un número a adivinar entre el 1 y el 100: "))
+        while guess != CORRECTO and intentos < 10:
+            ask = input(f"Es más grande que {guess} ?\n")
+            if ask == "si":
+                bot = guess
+            elif ask == "no":
+                top = guess
+            guess = random.randint(bot + 1, top - 1)
+            intentos +=1
+        if guess == CORRECTO:
+            print(f"He adivinado el número ({guess}), y me han sobrado {10 - intentos} intentos.")
+        if intentos >= 10:
+            print("Te has quedado sin intentos")
+    except Exception as e:
+        if ValueError:
+            print("Por favor, introduce un valor numérico.")
+            continue
+        else:
+            print(e)
+    break
