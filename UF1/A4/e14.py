ff3adfe01fd51c48b1f3dcfaf14131f939157106
iand-itb <ian.diaz.7e5@itb.cat>
2023-12-13 11:41:01 +0100

    noggin

--- UF1/A4/e14.py
+++ UF1/A4/e14.py
@@ -1,10 +1,16 @@
-souH = float(input("Introduce el sueldo por hora: "))
-totalH = 0
-totalS = 0
+try:
+    souH = float(input("Introduce el sueldo por hora: "))
+    totalH = 0
+    totalS = 0
 
-for dia in range(1, 7):
-    horas = int(input(f"Horas trabajadas dia {dia}:\t"))
-    totalH += horas
-    totalS += horas * souH
+    for dia in range(1, 7):
+        horas = int(input(f"Horas trabajadas dia {dia}:\t"))
+        totalH += horas
+        totalS += horas * souH
 
-print(f"\nTotal horas trabajadas:\t{totalH}h\nTotal sueldo a cobrar:\t{totalS:.2f}€")
+    print(f"\nTotal horas trabajadas:\t{totalH}h\nTotal sueldo a cobrar:\t{totalS:.2f}€")
+except Exception as e:
+    if ValueError:
+        print("Introduce valores numéricos.")
+    else:
+        print(e)
