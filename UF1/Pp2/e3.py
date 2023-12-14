f4563f13fa5eee7ec1fc5b59c5eb947e40c00eee
iand-casa <ian.diaz.7e5@itb.cat>
2023-12-14 14:05:28 +0100

    examen ej 3 variables x,y definidas

--- UF1/Pp2/e3.py
+++ UF1/Pp2/e3.py
@@ -7,13 +7,17 @@ try:
     BLANC = "B"
     NEGRE = "N"
     pos = input("Introduce una posición para el alfil con dos números separados por un espacio (y x): ").split()
-    while int(pos[0]) < 1 or int(pos[0]) > 8 or int(pos[1]) > 8 or int(pos[1]) < 1:
+    y = int(pos[0])
+    x = int(pos[1])
+    while y < 1 or y > 8 or x > 8 or x < 1:
         pos = input("Introduce una posición para el alfil con dos números separados por un espacio (y x): ").split()
+        y = int(pos[0])
+        x = int(pos[1])
     for i in range(1, 9):
         for j in range(1, 9):
-            if i == int(pos[0]) and j == int(pos[1]):
+            if i == y and j == x:
                 print("*", end=" ")
-            elif j == int(pos[1]) - (int(pos[0]) - i) or j == int(pos[1]) + (int(pos[0]) - i):
+            elif j == x - (y - i) or j == x + (y - i):
                 print("*", end=" ")
             elif (i + j) % 2 == 0:
                 print(BLANC, end=" ")
