505cec619035496f26fbd089f48ceb685a291cc8
iand-itb <ian.diaz.7e5@itb.cat>
2023-12-14 12:43:28 +0100

    exammm

--- UF1/Pp2/pp2-diazIan_e3.py
+++ UF1/Pp2/pp2-diazIan_e3.py
@@ -1,14 +1,14 @@
 '''
 Ian Díaz Pérez
 14/12/2023
-.
+Programa que a partir de unas coordenadas del taulell d'escacs, dibuixa amb "*" el recorregut possible de un alfil en la posició determinada.
 '''
 try:
     BLANC = "B"
     NEGRE = "N"
-    pos = input("Introduce una posición para el alfil con dos números separados por un espacio: ").split()
+    pos = input("Introduce una posición para el alfil con dos números separados por un espacio (y x): ").split()
     while int(pos[0]) < 1 or int(pos[1]) > 8:
-        pos = input("Introduce una posición para el alfil con dos números separados por un espacio: ").split()
+        pos = input("Introduce una posición para el alfil con dos números separados por un espacio (y x): ").split()
     for i in range(1, 9):
         for j in range(1, 9):
             if i == int(pos[0]) and j == int(pos[1]):
