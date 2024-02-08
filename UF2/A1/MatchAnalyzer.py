081bc3cee7eda35084730b8b9e76a8ebf5d9b046
iand-itb <ian.diaz.7e5@itb.cat>
2024-02-08 13:05:02 +0100

    todo: oldPoints < currPoints

--- UF2/A1/MatchAnalyzer.py
+++ UF2/A1/MatchAnalyzer.py
@@ -6,19 +6,27 @@ def get_teams():
     t = []
     while len(t) != 2:
         t = input("Enter 2 teams: ").split(" ")
-        teams.append(t)
+        for team in t:
+            teams.append(team)
 
 
 def add_points():
+    currPoints = []
     while True:
+        oldPoints = currPoints
         currPoints = input("Enter points for teams now (enter -1 to stop): ").split(" ")
         if '-1' in currPoints:
             break
-        currPoints = [int(x) for x in currPoints]
-        if len(currPoints) != 2:
-            print("Please enter points for both teams.")
-        else:
-            points.append(currPoints)
+        currPoints = [int(point) for point in currPoints]
+        for point in currPoints:
+            for oldpoint in oldPoints:
+                if oldpoint > point:
+                    break
+                else:
+                    if len(currPoints) != 2:
+                        print("Please enter points for both teams.")
+                    else:
+                        points.append(currPoints)
 
 
 get_teams()
